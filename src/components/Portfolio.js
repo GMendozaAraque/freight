22ad56661dc54c8a6252b36a77e57f19e838cc3e
GMendozaAraque <108754722+GMendozaAraque@users.noreply.github.com>
2025-02-17 11:12:16 -0600

    New changes

--- src/components/Portfolio.js
+++ src/components/Portfolio.js
@@ -14,7 +14,7 @@ const Portfolio = () => {
                     <div className="grid sm:grid-cols-2 lg:grid-cols-4 gap-5">                            
                         <div className="bg-white transition-all ease-in-out duration-400  overflow-hidden text-gray-700 hover:scale-105 rounded-lg shadow-2xl p-3 min-h-max">
                             <div className="m-2 text-justify text-sm">
-                                <h4 className="font-semibold my-4 text-lg md:text-2xl text-center mb-4 h-12">Complete Contact List – All the Essentials for Only $40</h4>
+                                <h4 className="font-semibold my-4 text-lg md:text-2xl text-center mb-4 h-12">Full Contact List – All the Essentials for Only $40</h4>
                                 <p className="text-md font-medium leading-5 h-auto md:h-48">
                                 <br></br>For just $40, get a fully curated contact list featuring the company name, physical address, phone numbers, website, and email. Our advanced AI-powered process ensures that each list is hyperlocal, up-to-date, and ready for immediate outreach. Whether you're dialing, texting, emailing, or even sending direct mail, this complete list provides you with all the essential details to accelerate your sales and maximize your results.
                                 </p>
@@ -31,7 +31,7 @@ const Portfolio = () => {
                             <div className="m-2 text-justify text-sm">
                                 <h4 className="font-semibold my-4 text-lg md:text-2xl text-center mb-4 h-12">Basic Package – Essential Contact Lists for $100/month</h4>
                                 <p className="text-md font-medium leading-5 h-auto md:h-48">
-                                <br></br>For just $100/month, receive one meticulously curated contact list per week (total 4 per month) featuring targeted addresses and phone numbers for freight brokers. Perfect for rapid outreach, you can add websites and emails for only $10 per list or upgrade to full details with additional lists at $40 each.
+                                <br></br>For just $100/month, receive one meticulously curated contact list per week (total 4 per month) featuring targeted phone numbers for freight brokers. Perfect for rapid outreach, you can add addresses, websites and emails for only $10 per list or upgrade to full details with additional lists at $40 each.
                                 </p>
                                 <div className="flex justify-center my-4">
                                     <Link to="/get-demo" className="text-white bg-blue-900 hover:bg-blue-800 inline-flex items-center justify-center w-full px-6 py-3 my-4 text-lg shadow-xl rounded-xl">
@@ -57,7 +57,7 @@ const Portfolio = () => {
                             <div className="m-2 text-justify text-sm">
                             <h4 className="font-semibold my-4 text-lg md:text-2xl text-center mb-4 h-12">Standard Outreach Package – Elevate Your Prospecting for $150/Month</h4>
                                 <p className="text-md font-medium leading-5 h-auto md:h-48">
-                                <br></br> <br></br>For only $150/month, get two curated contact lists per week (total 8 per month). Half come with full details, while the rest feature phone numbers and addresses only. Upgrade any list to full details for just $5 or add extra lists with complete data at $30 each.
+                                <br></br> <br></br>For only $150/month, get two curated contact lists per week (total 8 per month). Half come with full details, while the rest feature phone numbers only. Upgrade any list to full details for just $5 or add extra lists with complete data at $30 each.
                                 </p>
                                 <div className="flex justify-center my-4">
                                     <Link to="/get-demo" className="text-white bg-blue-900 hover:bg-blue-800 inline-flex items-center justify-center w-full px-6 py-3 my-4 text-lg shadow-xl rounded-xl">
