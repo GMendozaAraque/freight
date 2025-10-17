f8e5762e72e98c4da625b3ddf5d0c286e403d758
GMendozaAraque <108754722+GMendozaAraque@users.noreply.github.com>
2025-10-17 02:04:32 -0600

    Update Hero.js

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -16,13 +16,12 @@ const Hero = () => {
                     <div id='hero' className="flex flex-col lg:flex-row py-8 justify-between text-center lg:text-left">
                         <div className="lg:w-1/2 flex flex-col justify-center" data-aos="zoom-in" data-aos-delay="200">
                             <h1 className="mb-5 md:text-5xl text-3xl font-bold text-blue-900">
-                            {/* We build digital solutions to help businesses scale */}
-                            Vende más seguros: Citas confirmadas con dueños de negocio latinos
+                            Still Relying on Referrals? Your Competitors Are Booking 3x Inspections Online.
                             </h1>
-                            <div className="text-xl font-semibold tracking-tight mb-5 text-gray-500">¿Cansado de perder tiempo con leads? Te entregamos citas confirmadas cara a cara con dueños de negocio latinos en tu condado, listos para atenderte. Olvídate de la prospección; nosotros te conectamos directamente con el tomador de decisión en un mercado clave y con alta necesidad. Invierte tu tiempo en vender, no en buscar.</div>
+                            <div className="text-xl font-semibold tracking-tight mb-5 text-gray-500">You offer free roof inspections, but the best leads are scrolling past. We specialize in turning Facebook and Instagram into a predictable appointment machine—no more guessing. We handle the setup and deliver interested people so you invest your time in closing, not chasing.</div>
                             <div className="mb-4 space-x-0 md:space-x-2 md:mb-8">
                                 <Link to="/contact" className="text-white bg-blue-900 hover:bg-blue-800 inline-flex items-center justify-center w-full px-6 py-3 my-4 text-lg shadow-xl rounded-2xl sm:w-auto sm:mb-0">
-                                    Contáctanos
+                                    Contact Us
                                     <svg className="w-4 h-4 ml-1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor"><path fillRule="evenodd" d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z" clipRule="evenodd"></path></svg>
                                 </Link>
                                 {/* <Link to="/contact" className="text-white bg-blue-900 hover:bg-blue-800 inline-flex items-center justify-center w-full px-6 py-3 my-4 text-lg shadow-xl rounded-2xl sm:w-auto sm:mb-0">
